ae18bd5a49d02a9bab8cf83187ddb409f30afe4a
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.wireless.rit.edu>
2019-07-25 14:56:58 -0400

    Now I can reverse a number

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+      return Math.sign(n) * parseInt(reversed);
+}
 
 module.exports = reverseInt;
