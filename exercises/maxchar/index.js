fe4d3287814f5afb6f6428611b8d25f65f236210
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.wireless.rit.edu>
2019-07-25 16:23:07 -0400

    got an idea of how to tackle string characters problems

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+   let obj = {};
+   let max = 0;
+   let maxChar = '';
+
+  for(let char of str){
+    obj[char] ? obj[char]++ : obj[char] = 1;
+  }
+
+  for(let objs in obj){
+    if(obj[objs]>max){
+      max = obj[objs];
+      maxChar = objs;
+    }
+  }
+  return maxChar;
+  console.log(maxChar);
+
+}
 
 module.exports = maxChar;
