b2a596cc250ada42be96630a3305975d1a8d4909
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.wireless.rit.edu>
2019-10-09 15:56:54 -0400

    back to learing this again

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,70 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// My own solution
+
+// for(let row = 0; row <n; row++){
+//   let stair = '';
+
+//   for(let column = 0; column <n; column++){
+//     if(column <= row){
+//       stair += '#';
+//     }else{
+//       stair += ' ';
+//     }
+//   }
+//   console.log(reverseString(stair).concat(stair.slice(1)));
+// }
+
+// }
+// function reverseString(str) {
+// return str
+//   .split('')
+//   .reverse()
+//   .join('');
+
+// }
+
+// solution 1
+// function pyramid(n) {
+
+//   const midpoint = Math.floor((2*n - 1)/2);
+//   for(let row=0; row < n; row ++){
+//     let level = '';
+
+//     for(let column=0; column < 2 * n-1; column ++){
+//       if(midpoint - row <= column && midpoint + row >= column){
+//         level += '#';
+//       }else{
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+  
+// }
+
+
+function pyramid(n, row=0, level = '') {
+ 
+  if ( n === row ){
+    return;
+  }
+
+  if(2 * n -1 === level.length){
+    console.log(level);
+    return pyramid(n, row+1);
+  }
+
+  const midpoint = Math.floor((2*n - 1)/2);
+  let add;
+
+  if(midpoint - row <= level.length && midpoint +row >= level.length){
+    add = '#';
+  }else{
+    add = ' ';
+  }
+    pyramid(n, row, level + add);
+  }
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//solution 1
+// for(let row = 0; row <n; row++){
+//   let stair = '';
+//
+//   for(let column = 0; column <n; column++){
+//     if(column <= row){
+//       stair += '#';
+//     }else{
+//       stair += ' ';
+//     }
+//   }
+//   console.log(stair);
+// }
+
+function steps(n, row = 0, stair = '') {
+   if(n === row){
+     return;
+   }
+
+   if (n === stair.length){
+     console.log(stair);
+     return steps(n, row + 1);
+   }
+
+   if(stair.length <= row){
+     stair += '#';
+   }else{
+     stair += ' ';
+   }
+   steps(n,row,stair);
+}
 
 module.exports = steps;
